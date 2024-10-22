81cac3dced34225be01401efbd7c922afff46f12
rkgusx <namg673@newschool.edu>
2024-10-22 11:00:07 -0400

    javascript (main page)
    added zoom-in animation using javascript and css

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    const iphoneContainer = document.querySelector('.iphone-with-icons');
+    setTimeout(() => {
+      iphoneContainer.style.animation = 'scale-back 3s forwards';
+    }, 3000);
+  };
+  
