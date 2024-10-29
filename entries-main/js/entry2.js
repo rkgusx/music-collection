b5de120955167977ba01132c1fa36bd2810f6b6d
rkgusx <namg673@newschool.edu>
2024-10-28 20:04:24 -0400

    changed takoyaki image

--- entries-main/js/entry2.js
+++ entries-main/js/entry2.js
@@ -3,34 +3,30 @@ const backgroundImage = document.getElementById('background-image');
 const backgroundVideo = document.getElementById('background-video');
 const koalaImage = document.getElementById('koala-image');
 const audio = document.getElementById('audio');
-let isPlaying = false; // 현재 상태를 추적하는 변수
+let isPlaying = false;
 
 function toggleBackgroundAndAudio() {
     if (isPlaying) {
-        // 현재 상태가 재생 중이면
-        backgroundImage.style.opacity = '1'; // 배경 이미지로 전환
-        backgroundVideo.style.opacity = '0'; // 비디오 숨김
-        audio.pause(); // 오디오 멈춤
-        koalaImage.src = '../images/koala.png'; // 원래 koala 이미지로 변경
-        backgroundVideo.pause(); // 비디오 멈춤
+        backgroundImage.style.opacity = '1';
+        backgroundVideo.style.opacity = '0';
+        audio.pause();
+        koalaImage.src = '../images/koala.png';
+        backgroundVideo.pause();
     } else {
-        // 현재 상태가 정지 중이면
-        backgroundImage.style.opacity = '0'; // 비디오로 전환
-        backgroundVideo.style.opacity = '1'; // 비디오 보이기
-        audio.play(); // 오디오 재생
-        koalaImage.src = '../images/koala-eyes-closed.png'; // koala-eyes-closed 이미지로 변경
-        backgroundVideo.play(); // 비디오 재생
+        backgroundImage.style.opacity = '0';
+        backgroundVideo.style.opacity = '1';
+        audio.play();
+        koalaImage.src = '../images/koala-eyes-closed.png';
+        backgroundVideo.play();
     }
-    isPlaying = !isPlaying; // 상태 반전
+    isPlaying = !isPlaying;
 }
 
-// koala-nose 클릭 이벤트
 koalaNose.addEventListener('click', toggleBackgroundAndAudio);
 
-// 스페이스 바 누르면 koala_nose 클릭 이벤트 트리거
 document.addEventListener('keydown', function(event) {
     if (event.code === 'Space') {
-        event.preventDefault(); // 스페이스 바의 기본 동작 방지
-        toggleBackgroundAndAudio(); // 상태 전환 함수 호출
+        event.preventDefault();
+        toggleBackgroundAndAudio();
     }
 });
